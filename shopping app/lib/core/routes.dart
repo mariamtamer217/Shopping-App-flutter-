e46411114482fee2141a56372fd2e36c292471b5
mariamtamer217 <mariomatota07@gmail.com>
2025-08-25 10:13:32 +0300

    Delete shopping app directory

--- shopping app/lib/core/routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const welcome = '/';
-  static const signIn = '/signin';
-  static const signUp = '/signup';
-  static const home = '/home';
-  // In your routes.dart file
-  static const String cart = '/cart';
-}
